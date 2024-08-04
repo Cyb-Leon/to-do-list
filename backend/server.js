f43dd8b4f59cd5c5bd297e8b4290f11f74cb6438
leon <44578198+Cyb-Leon@users.noreply.github.com>
2024-08-04 23:59:09 +0200

    middleware testing

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,37 @@
 //using Express Js to create server
 import express from "express";
-import morgan from "morgan";
+import bodyParser from "body-parser";
+import { dirname} from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //instance of our server called ServerApp
 const serverApp = express();
 const port = 3001;
 
-//middleware logging Morgan
-serverApp.use(morgan("tiny"));
+
+//created express middleware logger
+const LeoLogger = (req,res, next) => {
+    console.log("Request method: " + req.method);
+    next();
+}
+
+
+//middleware logging LeoLogger
+serverApp.use(LeoLogger);
+serverApp.use(express.urlencoded({extended: true}));
+
 
 serverApp.get("/",(req,res)=>{
-    res.send("Sho dawg");
+    res.sendFile(`${__dirname}/public/index.html`);
+})
+
+
+serverApp.post("/tapin",(req,res)=>{
+    //mix masala the data and shit it out!
+
+console.log(req.body);
 })
 
 serverApp.listen(port, () => console.log(`server listening on port ${port}!`))
