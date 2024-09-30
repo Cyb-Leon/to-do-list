d995e3b738a1e36ef107ce1c1f819586471f5335
leon <44578198+Cyb-Leon@users.noreply.github.com>
2024-09-30 21:33:30 +0200

    fix

---
+++ src/components/layout/Input.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function Input(props) {
+    return (
+        <input
+        type= {props.type}
+        className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+        id= {props.id}
+        placeholder= {props.placeholder} />
+    )
+}
+
+export default Input;
