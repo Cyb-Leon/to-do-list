2a6a844e9016f39c3242b3333667d98f85622625
leon <44578198+Cyb-Leon@users.noreply.github.com>
2024-10-02 20:09:09 +0200

    adding devDepens

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,104 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./layout/header";
 import Footer from "./layout/footer";
 import Login from "./login/Login";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./layout/CreateArea";
 import { Input, Ripple, initTWE } from "tw-elements";
 import Signup from "./register/Signup";
 
-// const usersDB = new pg.Client({
-//   user: process.env.gresUsr,
-//   host: process.env.gresHOST,
-//   database: process.env.gresDB,
-//   password:process.env.gresPSW,
-//   port: process.env.gressPORT
-// })
-// usersDB.connect();
-
-
-
-
-function createNotes(noteItem) {
-  return (
-    <Note
-      key={noteItem.key}
-      title={noteItem.title}
-      content={noteItem.content}
-    />
-  );
-}
-
-let isLoggedIn = false;
-
-// const users =[
-//   {
-//     id: 1,
-//     username: "Leon",
-//     userpass: 12345
-//   },
-//   {
-//     id:2,
-//     username: "Zama",
-//     userpass: 12344
-//   }
-
-// ]
-
-
- function renderConditionally() {
-
-  //get users
-  // const users = usersDB.query(
-  //   "SELECT email, password FROM users WHERE email=($1) AND password=($2)", [props.username, props.psw], (err, results) => {
-  //     if (err) {
-  //       return err;
-  //     } else {
-
-  //     }
-  //   }
-  // )
-
-  //ternary operator
-  return (!isLoggedIn ? (<div> <Login/> <Footer/> </div>) :(<div> <Header /> {notes.map(createNotes)} <Footer /> </div>))
-}
-
 function App() {
   // const results = users.map((user) => user.username = ${givenUser})
-useEffect(()=>{
-  initTWE({ Input, Ripple });
-},[])
- if (isLoggedIn === undefined) {
-   return <div>
-    <Signup/>
-   </div> 
- }else{
-  return <div>
-  {renderConditionally()}
 
-</div>
- }
+  //to be able to use effects on component
+  useEffect(() => {
+    initTWE({ Input, Ripple });
+  }, []);
 
+  //array of notes to be update
+  const [notes,setNote] = useState([]);
+  
+  function addNote(newNote) {
+    setNote(prevNotes =>{
+    return  [ ...prevNotes,newNote];
+    });
+  }
 
+  //delete a note
+  function deleteNote(id) {
+    //set the value from using the useState
+    setNote(prevNotes => {
+      return prevNotes.filter((noteItem,index) =>{
+        return index !== id
+      })
+    })
+    console.log("i was trigged to delete from Note.jsx");
+  }
+
+  return (
+    <div>
+      {" "}
+      <Header /> <CreateArea onAdd={addNote} />{" "}
+      {notes.map((noteItem,index) =>{
+        return  <Note key={index} id={index} title= {noteItem.title} content= {noteItem.content} onDelete={deleteNote}/>
+      })}
+      <Footer />{" "}
+    </div>
+  );
+
+  //  if (isLoggedIn === undefined) {
+  //    return <div>
+  //     <Signup/>
+  //    </div>
+  //  }else{
+  //   return <div>
+  //   {renderConditionally()}
+  // </div>
+  //  }
+
+  // function createNotes(noteItem) {
+  //   return (
+  //     <Note
+  //       key={noteItem.key}
+  //       title={noteItem.title}
+  //       content={noteItem.content}
+  //     />
+  //   );
+  // }
+
+  let isLoggedIn = true;
+
+  // const users =[
+  //   {
+  //     id: 1,
+  //     username: "Leon",
+  //     userpass: 12345
+  //   },
+  //   {
+  //     id:2,
+  //     username: "Zama",
+  //     userpass: 12344
+  //   }
+
+  // ]
+
+  //  function renderConditionally() {
+
+  //   //get users
+  //   // const users = usersDB.query(
+  //   //   "SELECT email, password FROM users WHERE email=($1) AND password=($2)", [props.username, props.psw], (err, results) => {
+  //   //     if (err) {
+  //   //       return err;
+  //   //     } else {
+
+  //   //     }
+  //   //   }
+  //   // )
+
+  //   //ternary operator
+  //   return (!isLoggedIn ? (<div> <Login/> <Footer/> </div>) :(<div> <Header /> <CreateArea/> <Note key={1} title="Note title"/><Footer /> </div>))
+  // }
 }
 
 export default App;
---
+++ src/components/layout/CreateArea.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+
+    const [note,setNote] = useState({
+        title: "",
+        content: ""
+    }) //objects
+
+    function handleChange(event) {
+        const {name,value} = event.target; //event listeners/handlers
+    
+        setNote(prevNote =>{
+            return {
+                ...prevNote, //Spread Operator
+                [name]:value
+            };
+        });
+    }
+
+    //submit note
+    function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+            title: "",
+            content:""
+        })
+
+        //prevent window reload when content changes.
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+        <form>
+          <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+          <textarea name="content" onChange={handleChange}  value={note.content} placeholder="Take a note..." rows="3" />
+          <button onClick={submitNote}>Add</button>
+        </form>
+      </div>
+    )
+}
+
+export default CreateArea;
